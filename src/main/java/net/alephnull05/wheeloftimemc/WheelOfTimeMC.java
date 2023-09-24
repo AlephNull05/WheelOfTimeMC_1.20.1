89a2c40e638957c4d3631274047b7eb0c261431b
AlephNull05 <sahit.jayaweera@gmail.com>
2023-09-24 10:27:18 -0700

    Test

---
+++ src/main/java/net/alephnull05/wheeloftimemc/WheelOfTimeMC.java
@@ -0,0 +1,59 @@
+package net.alephnull05.wheeloftimemc;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(net.alephnull05.wheeloftimemc.WheelOfTimeMC.MOD_ID)
+public class WheelOfTimeMC
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "wheeloftimemc";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public WheelOfTimeMC()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
